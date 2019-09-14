7f638ebee9f2105d51768ef487a245b7c1cf40a1
Julien CREUX <jcreux@e1r3p3.42.fr>
2019-09-14 21:45:04 +0200

    ajout de la possibilite de changer de set de couleur

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jcreux <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 13:01:16 by jcreux            #+#    #+#             */
-/*   Updated: 2019/09/11 20:02:15 by jcreux           ###   ########.fr       */
+/*   Updated: 2019/09/14 21:42:31 by jcreux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 #include <stdlib.h>
 #include "fractol.h"
 
-static void	zoom(t_mlx *mlx, double c)
+static void	zoom(t_mlx *mlx, int x, int y, double k)
 {
 	double	r1;
 	double	r2;
 
-	r1 = (mlx->x_max - mlx->x_min) * c;
-	r2 = (mlx->y_max - mlx->y_min) * c;
+	(void)x;
+	(void)y;
+	r1 = (mlx->x_max - mlx->x_min) * k;
+	r2 = (mlx->y_max - mlx->y_min) * k;
 	mlx->x_max -= (mlx->x_max - mlx->x_min) - r1;
 	mlx->y_max -= (mlx->y_max - mlx->y_min) - r2;
 }
@@ -32,6 +34,16 @@ static int	leave(t_mlx *mlx)
 	exit(1);
 }
 
+static int	mouse_press(int key, int x, int y, t_mlx *mlx)
+{
+	if (key == 4)
+		zoom(mlx, x, y, 1.05);
+	else if (key == 5)
+		zoom(mlx, x, y, 0.95);
+	create_fractal(mlx);
+	return (0);
+}
+
 static int	key_press(int key, t_mlx *mlx)
 {
 	if (key == ESC)
@@ -40,19 +52,15 @@ static int	key_press(int key, t_mlx *mlx)
 		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
 		exit(1);
 	}
-	else if (key == 69)
-		zoom(mlx, 0.95);
-	else if (key == 78)
-		zoom(mlx, 1.05);
 	else if (key == 126)
 		mlx->iter++;
 	else if (key == 125 && mlx->iter > 0)
 		mlx->iter--;
-	else if (key == 83)
+	else if (key == 18)
 		mlx->color_set = 1;
-	else if (key == 84)
+	else if (key == 19)
 		mlx->color_set = 2;
-	else if (key == 85)
+	else if (key == 20)
 		mlx->color_set = 3;
 	create_fractal(mlx);
 	return (0);
@@ -76,6 +84,7 @@ int			main(int ac, char **av)
 	mlx.img_data = mlx_get_data_addr(mlx.img_ptr, &mlx.bpp, &mlx.sl, &mlx.en);
 	create_fractal(&mlx);
 	mlx_hook(mlx.win_ptr, 2, 0, key_press, &mlx);
+	mlx_hook(mlx.win_ptr, 4, 0, mouse_press, &mlx);
 	mlx_hook(mlx.win_ptr, 17, 0, leave, &mlx);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
