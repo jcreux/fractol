2a0dd01b2a87ea357772492211c0a6497de0760f
Julien CREUX <jcreux@e1r12p14.42.fr>
2019-09-18 00:53:32 +0200

    changement des sets de couleur

--- color.c
+++ color.c
@@ -6,7 +6,7 @@
 /*   By: jcreux <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 19:07:46 by jcreux            #+#    #+#             */
-/*   Updated: 2019/09/11 20:30:24 by jcreux           ###   ########.fr       */
+/*   Updated: 2019/09/18 00:51:31 by jcreux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,44 +30,74 @@ static void	fill_pixel(t_mlx *mlx, int x, int y, unsigned int color)
 
 static void	first_set(t_mlx *mlx, int x, int y)
 {
-	if (mlx->i % 5 == 0)
-		fill_pixel(mlx, x, y, 0xfec83d);
-	else if (mlx->i % 5 == 1)
-		fill_pixel(mlx, x, y, 0xfec228);
-	else if (mlx->i % 5 == 2)
-		fill_pixel(mlx, x, y, 0xe4ae24);
-	else if (mlx->i % 5 == 3)
-		fill_pixel(mlx, x, y, 0xcb9b20);
-	else if (mlx->i % 5 == 4)
-		fill_pixel(mlx, x, y, 0xb1871c);
+		if (mlx->i % 10 == 0)
+		fill_pixel(mlx, x, y, 0x000000);
+	else if (mlx->i % 10 == 1)
+		fill_pixel(mlx, x, y, 0x0a0d17);
+	else if (mlx->i % 10 == 2)
+		fill_pixel(mlx, x, y, 0x151a2e);
+	else if (mlx->i % 10 == 3)
+		fill_pixel(mlx, x, y, 0x202745);
+	else if (mlx->i % 10 == 4)
+		fill_pixel(mlx, x, y, 0x2a355d);
+	else if (mlx->i % 10 == 5)
+		fill_pixel(mlx, x, y, 0x8466a3);
+	else if (mlx->i % 10 == 6)
+		fill_pixel(mlx, x, y, 0x704c93);
+	else if (mlx->i % 10 == 7)
+		fill_pixel(mlx, x, y, 0x5b3284);
+	else if (mlx->i % 10 == 8)
+		fill_pixel(mlx, x, y, 0x471975);
+	else if (mlx->i % 10 == 9)
+		fill_pixel(mlx, x, y, 0x330066);
 }
 
 static void	second_set(t_mlx *mlx, int x, int y)
 {
-	if (mlx->i % 5 == 0)
-		fill_pixel(mlx, x, y, 0xff0000);
-	else if (mlx->i % 5 == 1)
-		fill_pixel(mlx, x, y, 0xe50000);
-	else if (mlx->i % 5 == 2)
-		fill_pixel(mlx, x, y, 0xcc0000);
-	else if (mlx->i % 5 == 3)
-		fill_pixel(mlx, x, y, 0xb20000);
-	else if (mlx->i % 5 == 4)
-		fill_pixel(mlx, x, y, 0x990000);
+		if (mlx->i % 10 == 0)
+		fill_pixel(mlx, x, y, 0x000000);
+	else if (mlx->i % 10 == 1)
+		fill_pixel(mlx, x, y, 0x005959);
+	else if (mlx->i % 10 == 2)
+		fill_pixel(mlx, x, y, 0x006666);
+	else if (mlx->i % 10 == 3)
+		fill_pixel(mlx, x, y, 0x007373);
+	else if (mlx->i % 10 == 4)
+		fill_pixel(mlx, x, y, 0x008080);
+	else if (mlx->i % 10 == 5)
+		fill_pixel(mlx, x, y, 0x405054);
+	else if (mlx->i % 10 == 6)
+		fill_pixel(mlx, x, y, 0x516469);
+	else if (mlx->i % 10 == 7)
+		fill_pixel(mlx, x, y, 0x627378);
+	else if (mlx->i % 10 == 8)
+		fill_pixel(mlx, x, y, 0x859296);
+	else if (mlx->i % 10 == 9)
+		fill_pixel(mlx, x, y, 0xcad0d2);
 }
 
 static void	third_set(t_mlx *mlx, int x, int y)
 {
-	if (mlx->i % 5 == 0)
-		fill_pixel(mlx, x, y, 0x327eac);
-	else if (mlx->i % 5 == 1)
-		fill_pixel(mlx, x, y, 0x196fa2);
-	else if (mlx->i % 5 == 2)
-		fill_pixel(mlx, x, y, 0x005f98);
-	else if (mlx->i % 5 == 3)
-		fill_pixel(mlx, x, y, 0x005588);
-	else if (mlx->i % 5 == 4)
-		fill_pixel(mlx, x, y, 0x004c79);
+		if (mlx->i % 10 == 0)
+		fill_pixel(mlx, x, y, 0x000000);
+	else if (mlx->i % 10 == 1)
+		fill_pixel(mlx, x, y, 0x050017);
+	else if (mlx->i % 10 == 2)
+		fill_pixel(mlx, x, y, 0x1d192e);
+	else if (mlx->i % 10 == 3)
+		fill_pixel(mlx, x, y, 0x363245);
+	else if (mlx->i % 10 == 4)
+		fill_pixel(mlx, x, y, 0x504c5c);
+	else if (mlx->i % 10 == 5)
+		fill_pixel(mlx, x, y, 0xe51284);
+	else if (mlx->i % 10 == 6)
+		fill_pixel(mlx, x, y, 0xff1493);
+	else if (mlx->i % 10 == 7)
+		fill_pixel(mlx, x, y, 0xff5b9d);
+	else if (mlx->i % 10 == 8)
+		fill_pixel(mlx, x, y, 0xff42a8);
+	else if (mlx->i % 10 == 9)
+		fill_pixel(mlx, x, y, 0xff5ab3);
 }
 
 void		color(t_mlx *mlx, int x, int y)
